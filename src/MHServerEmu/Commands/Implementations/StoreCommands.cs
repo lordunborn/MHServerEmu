9bed1d1684f3458c64b3f6f03da3f590a8b44d90
mtzimas92 <142402610+mtzimas92@users.noreply.github.com>
2025-05-04 16:18:43 -0400

    Update StoreCommands.cs

--- src/MHServerEmu/Commands/Implementations/StoreCommands.cs
+++ src/MHServerEmu/Commands/Implementations/StoreCommands.cs
@@ -64,39 +64,5 @@ namespace MHServerEmu.Commands.Implementations
             return $"Acquired {amount} Gs.";
         }
 
-        [Command("convert", "Converts Eternity Splinters to Gs. Default is 1.\nUsage: store convert [amount]", AccountUserLevel.User)]
-        public string Convert(string[] @params, FrontendClient client)
-        {
-            if (client == null) return "You can only invoke this command from the game.";
-
-            int NumConverted = 1;
-            if (@params.Length > 0 && int.TryParse(@params[0], out int specifiedAmount))
-            {
-                if (specifiedAmount <= 0) return "Amount can't be 0 or negative.";
-                NumConverted = specifiedAmount;
-            }
-
-            CommandHelper.TryGetPlayerConnection(client, out PlayerConnection playerConnection);
-            Player player = playerConnection.Player;
-
-            PropertyId esPropId = new(PropertyEnum.Currency, GameDatabase.CurrencyGlobalsPrototype.EternitySplinters);
-
-            long esBalance = player.Properties[esPropId];
-            if (esBalance < NumConverted)
-                return $"You need at least {NumConverted} Eternity Splinters to convert them to Gs.";
-
-            var config = ConfigManager.Instance.GetConfig<BillingConfig>();
-            long gAmount = Math.Max((long)(NumConverted * config.ESToGazillioniteConversionRatio), 0);
-            if (gAmount == 0)
-                return "Current server settings do not allow Eternity Splinter to G conversion.";
-
-            if (player.AcquireGazillionite(gAmount) == false)
-                return "Failed to acquire Gs.";
-
-            player.Properties.AdjustProperty(-NumConverted, esPropId);
-
-            return $"Converted {NumConverted} Eternity Splinters to {gAmount} Gs.";
-        }
-
     }
 }
